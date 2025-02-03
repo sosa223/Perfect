1abcc133356c397fb620ccfcfc7d99ac08cd9adf
Chief <75649719+sosa223@users.noreply.github.com>
2025-02-02 23:05:31 -0500

    main.c

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int isPerfect(int num) {
+    int sum = 1; 
+    for (int n = 2; n * n <= num; n++) {
+    if (num % n == 0) {
+    sum += n;
+    if (n != num / n) {
+      sum += num / n;
+   }
+
+}
+    }
+    return sum == num;
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (isPerfect(number)) {
+        printf("Perfect Number");
+    } else {
+        printf("Not A Perfect Number");
+  }
+
+    return 0;
 }
